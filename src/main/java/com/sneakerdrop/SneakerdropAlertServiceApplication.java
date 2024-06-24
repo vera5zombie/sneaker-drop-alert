534af446af75517164ba1be43e2570131da09c42
vera5zombie <vera5zombie@gmail.com>
2024-06-23 23:49:38 -0400

    Add initial files

---
+++ src/main/java/com/sneakerdrop/SneakerdropAlertServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sneakerdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerdropAlertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerdropAlertServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sneakerdrop/SneakerdropAlertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneakerdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerdropServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
