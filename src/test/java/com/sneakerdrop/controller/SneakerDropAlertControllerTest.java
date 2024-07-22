4c62265aa8fa3a595f3051ccf8b69881d459caee
vera5zombie <vera5zombie@gmail.com>
2024-07-21 22:41:09 -0400

    Updated names, method signatures and add unit test class

---
+++ src/test/java/com/sneakerdrop/controller/SneakerDropAlertControllerTest.java
@@ -0,0 +1,20 @@
+package com.sneakerdrop.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SneakerDropAlertControllerTest {
+
+    @Test
+    void getSites() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void unsubscribe() {
+    }
+}
